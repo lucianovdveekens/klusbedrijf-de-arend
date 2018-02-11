d5a88366f8bf0aa7de08bdc7f9b42ec3335da9e8
Luciano van der Veekens <lucianovdveekens@gmail.com>
2018-02-11 12:51:10 +0100

    Change the directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,120 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var filter = require('gulp-filter');
+var del = require('del');
 var pkg = require('./package.json');
 
-// Set the banner content
-var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  ''
-].join('');
-
 gulp.task('sass', function() {
-  return gulp.src('scss/*.scss')
-    .pipe(sass())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+  return gulp.src('app/scss/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('app/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src(['css/*.css', '!css/*.min.css'])
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+  return gulp.src('app/css/*.css')
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  }))
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/freelancer.js')
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+  return gulp.src('tmp/js/*.js')
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 });
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
+gulp.task('vendors', function() {
   gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
-
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
-
+    'node_modules/bootstrap/dist/**/*',
+    '!**/npm.js',
+    '!**/bootstrap-theme.*',
+    '!**/*.map'
+  ])
+  .pipe(gulp.dest('app/vendor/bootstrap'))
+  
+  gulp.src([
+    'node_modules/jquery/dist/jquery.js', 
+    'node_modules/jquery/dist/jquery.min.js'
+  ])
+  .pipe(gulp.dest('app/vendor/jquery'))
+  
   gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
-
+  .pipe(gulp.dest('app/vendor/jquery-easing'))
+  
   gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'))
+  .pipe(gulp.dest('app/vendor/magnific-popup'))
+  
+  gulp.src([
+    'node_modules/font-awesome/**',
+    '!node_modules/font-awesome/**/*.map',
+    '!node_modules/font-awesome/.npmignore',
+    '!node_modules/font-awesome/*.txt',
+    '!node_modules/font-awesome/*.md',
+    '!node_modules/font-awesome/*.json'
+  ])
+  .pipe(gulp.dest('app/vendor/font-awesome'))
+  
+  gulp.src([
+    'bower_components/slick-carousel/slick/**',
+    '!bower_components/slick-carousel/slick/slick.js',
+    '!bower_components/slick-carousel/slick/*.scss', 
+    '!bower_components/slick-carousel/slick/*.rb', 
+  ])
+  .pipe(gulp.dest('app/vendor/slick-carousel'))
 
   gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('vendor/font-awesome'))
+    'bower_components/slick-lightbox/dist/slick-lightbox.min.js', 
+    'bower_components/slick-lightbox/dist/slick-lightbox.css'
+  ])
+  .pipe(gulp.dest('app/vendor/slick-lightbox'))
 })
 
 // Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'vendors']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: '.',
-      index: 'index.html'
+      baseDir: 'app'
     },
   })
 })
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['minify-css']);
-  gulp.watch('js/*.js', ['minify-js']);
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/css/**/*.css', ['minify-css']);
+  gulp.watch('app/js/**/*.js', ['minify-js']);
+  gulp.watch('app/*.html', browserSync.reload);
+  // is this needed? Doesn't minify-js already trigger a reload?
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
